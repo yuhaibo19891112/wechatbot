607c44799131f37d4a5de23e744ff0940d06f836
yuhaibo19891112 <yuhaibo19891112@gmail.com>
2022-12-13 14:22:09 +0800

    Update handler.go

--- handlers/handler.go
+++ handlers/handler.go
@@ -49,6 +49,17 @@ func Handler(msg *openwechat.Message) {
 				log.Printf("add friend agree error : %v", err)
 				return
 			}
+			// 加好友默认提示语
+			img, err := loadRemoteImg(config.Config.QunUrl, "qun.png")
+			reply := "自动回复：由于线路限流，暂时关闭私聊功能，非常抱歉！\n \n不过我们仍然支持群聊，建议您邀请朋友一起关注【V起来】，然后拉机器人进群，进行群聊。或者直接加入官方群体>验，关注进群方式请点击：\n \nhttps://mp.weixin.qq.com/s/n-zjrRsa8lNrzhZV9iFMww"
+			if img != nil {
+				reply = "自动回复：由于线路限流，暂时关闭私聊功能，非常抱歉！\n \n不过我们仍然支持群聊，建议您邀请朋友一起关注【V起来】，然后拉机器人进群，进行群聊。或者直接加入官方群体验。进群方式请扫下方二维码"
+			}
+			_, err = friend.SendText(reply)
+			if img != nil {
+				_, err = friend.SendImage(img)
+			}
+			return
 		}
 	}
 
