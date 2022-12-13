222bdeb8c0b30d939cccb3d5e89dd5720045c33e
yuhaibo19891112 <yuhaibo19891112@gmail.com>
2022-12-13 11:31:22 +0800

    Update group_msg_handler.go

--- handlers/group_msg_handler.go
+++ handlers/group_msg_handler.go
@@ -22,10 +22,10 @@ func (g *GroupMessageHandler) handle(msg *openwechat.Message) error {
 	// 自己加入群聊
 	joinTip := config.Config.JoinGroupTip
 	if selfJoinGroup(msg) && joinTip != ""{
-		downloadImg(config.Config.QunUrl, "qun.jpg")
-		img, err := os.Open("qun.jpg")
+		downloadImg(config.Config.QunUrl, "qun.png")
+		img, err := os.Open("qun.png")
 		if err != nil {
-			log.Printf("获取qun.jpg图片异常， %v", err)
+			log.Printf("获取qun.png图片异常， %v", err)
 		}
 		if err != nil {
 			msg.ReplyText( joinTip + "\n https://mp.weixin.qq.com/s/n-zjrRsa8lNrzhZV9iFMww")
