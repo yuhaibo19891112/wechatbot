1e3dabb5b012c57cd189a51c1a60c67dc5422ae4
yuhaibo19891112 <yuhaibo19891112@gmail.com>
2023-01-03 11:19:27 +0800

    Update handler.go

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,67 +1,122 @@
 package handlers
 
 import (
-	"github.com/869413421/wechatbot/service"
+	"github.com/869413421/wechatbot/config"
 	"github.com/eatmoreapple/openwechat"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
-// MessageHandlerInterface 消息处理接口
-type MessageHandlerInterface interface {
-	handle(*openwechat.Message) error
-	ReplyText(*openwechat.Message) error
+var _ MessageHandlerInterface = (*UserMessageHandler)(nil)
+
+// UserMessageHandler 私聊消息处理
+type UserMessageHandler struct {
 }
 
-type HandlerType string
-
-const (
-	GroupHandler = "group"
-	UserHandler  = "user"
-)
-
-// handlers 所有消息类型类型的处理器
-var handlers map[HandlerType]MessageHandlerInterface
-var UserService service.UserServiceInterface
-
-func init() {
-	handlers = make(map[HandlerType]MessageHandlerInterface)
-	handlers[GroupHandler] = NewGroupMessageHandler()
-	handlers[UserHandler] = NewUserMessageHandler()
-
-	UserService = service.NewUserService()
+// handle 处理消息
+func (g *UserMessageHandler) handle(msg *openwechat.Message) error {
+	if msg.IsText() {
+		return g.sendMsgCommand(msg)
+	}
+	return nil
 }
 
-// Handler 全局处理入口
-func Handler(msg *openwechat.Message) {
-	log.Printf("hadler Received msg : %v", msg.Content)
-	// 处理群消息
-	if msg.IsSendByGroup() {
-		handlers[GroupHandler].handle(msg)
-		return
+// NewUserMessageHandler 创建私聊处理器
+func NewUserMessageHandler() MessageHandlerInterface {
+	return &UserMessageHandler{}
+}
+
+func (g *UserMessageHandler) sendMsgCommand(msg *openwechat.Message) error {
+	// 接收私聊消息
+	sender, _ := msg.Sender()
+	log.Printf("--------------user: %s", sender.NickName)
+	if !strings.Contains(config.Config.SystemUser, sender.NickName) {
+		return nil
+	}
+	if !strings.HasPrefix(msg.Content,"发消息：") {
+		return nil
+	}
+	msgCmmd := strings.ReplaceAll(msg.Content, "发消息：", "")
+	self, _ := msg.Bot.GetCurrentUser()
+	groups, _ := self.Groups()
+	for i := 0; i < len(groups); i++ {
+		time.Sleep(2 * time.Second)
+		groups[i].SendText(msgCmmd)
+	}
+	return nil
+}
+
+// ReplyText 发送文本消息到群
+func (g *UserMessageHandler) ReplyText(msg *openwechat.Message) error {
+	// 接收私聊消息
+	sender, err := msg.Sender()
+	log.Printf("Received User %v Text Msg : %v", sender.NickName, msg.Content)
+
+
+        // 设置上下文，回复用户
+	img, err := loadRemoteImg(config.Config.QunUrl, "qun.png")
+	reply := "自动回复：由于线路限流，暂时关闭私聊功能，非常抱歉！\n \n不过我们仍然支持群聊，建议您邀请朋友一起关注【V起来】，然后拉机器人进群，进行群聊。或者直接加入官方群体>验，关注进群方式请点击：\n \nhttps://mp.weixin.qq.com/s/n-zjrRsa8lNrzhZV9iFMww"
+	if img != nil {
+		reply = "自动回复：由于线路限流，暂时关闭私聊功能，非常抱歉！\n \n不过我们仍然支持群聊，建议您邀请朋友一起关注【V起来】，然后拉机器人进群，进行群聊。或者直接加入官方群体验。进群方式请扫下方二维码"
+	}
+	_, err = msg.ReplyText(reply)
+	if img != nil {
+		_, err = msg.ReplyImage(img)
+	}
+	if err != nil {
+		log.Printf("response user error: %v \n", err)
+	}
+	return err
+
+/*
+	// 获取上下文，向GPT发起请求
+	requestText := strings.TrimSpace(msg.Content)
+	requestText = strings.Trim(msg.Content, "\n")
+
+	requestText = UserService.GetUserSessionContext(sender.ID()) + requestText
+	reply, err := gtp.Completions(requestText)
+	if err != nil {
+		log.Printf("gtp request error: %v \n", err)
+		errorTip := "机器人累了要歇会儿，我很快就能V起来……"
+		if reply == "429" {
+		    warnFriend(msg)
+		    errorTip = errorTip + "!!!!!!!"
+		}
+		msg.ReplyText(errorTip)
+		return err
+	}
+	if reply == "" {
+		return nil
 	}
 
-	// 好友申请
-	/*if msg.IsFriendAdd() {
-		if config.Config.AutoPass {
-			friend, err := msg.Agree("你好我是【V起来】微信群聊版 ChatGPT机器人。\n 请进群体验，私聊小窗不再回复：https://mp.weixin.qq.com/s/n-zjrRsa8lNrzhZV9iFMww")
-			if err != nil {
-				log.Printf("add friend agree error : %v", err)
-				return
-			}
-			// 加好友默认提示语
-			img, err := loadRemoteImg(config.Config.QunUrl, "qun.png")
-			reply := "自动回复：由于线路限流，暂时关闭私聊功能，非常抱歉！\n \n不过我们仍然支持群聊，建议您邀请朋友一起关注【V起来】，然后拉机器人进群，进行群聊。或者直接加入官方群体>验，关注进群方式请点击：\n \nhttps://mp.weixin.qq.com/s/n-zjrRsa8lNrzhZV9iFMww"
-			if img != nil {
-				reply = "自动回复：由于线路限流，暂时关闭私聊功能，非常抱歉！\n \n不过我们仍然支持群聊，建议您邀请朋友一起关注【V起来】，然后拉机器人进群，进行群聊。或者直接加入官方群体验。进群方式请扫下方二维码"
-			}
-			_, err = friend.SendText(reply)
-			if img != nil {
-				_, err = friend.SendImage(img)
-			}
-			return
-		}
-	}*/
+	// 设置上下文，回复用户
+	reply = strings.TrimSpace(reply)
+	reply = strings.Trim(reply, "\n")
+	UserService.SetUserSessionContext(sender.ID(), requestText, reply)
+	reply = "本消息由 V起来Bot 回复：\n " + reply
+	_, err = msg.ReplyText(reply)
+	if err != nil {
+		log.Printf("response user error: %v \n", err)
+	}
+	return err
+*/
+}
 
-	// 私聊
-	handlers[UserHandler].handle(msg)
+func loadRemoteImg(finalUrl string, savePath string) (*os.File, error) {
+	//读取url的信息，存入到文件
+	resp, err := http.Get(finalUrl)
+	if err != nil {
+		log.Printf("get remote img error, %v", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+	content, err := ioutil.ReadAll(resp.Body)
+	if err == nil {
+		ioutil.WriteFile(savePath, content, 0666)
+	}
+	return os.Open(savePath)
 }
