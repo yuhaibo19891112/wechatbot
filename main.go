831d7b17d82c7ffa606a4bd532abfcbde1d7be3c
michael510cj <64938222+michael510cj@users.noreply.github.com>
2023-01-13 23:09:43 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/869413421/wechatbot/bootstrap"
 	"github.com/869413421/wechatbot/botcron"
 	"github.com/869413421/wechatbot/config"
+	"github.com/869413421/wechatbot/services"
 )
 
 func main() {
@@ -13,6 +14,8 @@ func main() {
 	// 定时任务测试demo
 	task := func() {
 		fmt.Printf("cron test")
+		rulesConfig := services.NewRulesConfigService().GetNewsRulesConfig()
+		fmt.Printf("{},{}", rulesConfig.SendGroup, rulesConfig.SendTime)
 	}
 	go botcron.NewWeChatBotCron("*/5 * * * * ?", task)
 	// 定时任务测试结束
