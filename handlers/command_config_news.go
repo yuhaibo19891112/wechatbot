111462dc8d05fdf4b5c4e49704e7acc983e247b5
yuhaibo19891112 <yuhaibo19891112@gmail.com>
2023-01-16 10:37:27 +0800

    Update command_config_news.go

--- handlers/command_config_news.go
+++ handlers/command_config_news.go
@@ -2,12 +2,13 @@ package handlers
 
 import (
 	"encoding/json"
-	"github.com/869413421/wechatbot/bootstrap"
 	"github.com/869413421/wechatbot/botcron"
 	"github.com/869413421/wechatbot/config"
 	"github.com/eatmoreapple/openwechat"
 	"github.com/robfig/cron/v3"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"strings"
 	"time"
 )
@@ -18,6 +19,8 @@ var sendType = ""
 var sendName = ""
 
 var sendNewsCron *cron.Cron
+
+var self *openwechat.Bot
 
 // CommandConfigNewsHandler 新闻配置处理器
 type CommandConfigNewsHandler struct {
@@ -33,6 +36,7 @@ type ConfigNewsData struct {
 
 func (c CommandConfigNewsHandler) handle(message *openwechat.Message) error {
 	sender, _ := message.Sender()
+	self = message.Bot()
 	// 非系统用户直接返回，没有权限
 	if !strings.Contains(config.Config.SystemUser, sender.NickName) {
 		log.Printf("config news no system user")
@@ -72,15 +76,23 @@ func (c CommandConfigNewsHandler) handle(message *openwechat.Message) error {
 }
 
 func sendNewsTask() {
-	bot := bootstrap.WeChatBot()
-	user, err := bot.GetCurrentUser()
+	user, err := self.GetCurrentUser()
 	if err != nil {
 		log.Printf("get current user error, %s", err.Error())
 	}
+
 	if sendName == "" {
 		log.Printf("command config news sendName is blank")
 		return
 	}
+
+	newsStr := RemoteNews()
+	if newsStr == "" {
+		log.Printf("query news empty")
+		return
+	}
+	log.Printf("send news task, sendType:%s, sendName:%s", sendType, sendName)
+
 	// 发送好友
 	if "1" == sendType {
 		friends, _ := user.Friends()
@@ -91,9 +103,9 @@ func sendNewsTask() {
 
 		users := strings.Split(sendName, ";")
 		for i := 0; i < len(users); i++ {
-			temp := friends.GetByRemarkName(users[i])
+			temp := friends.GetByNickName(users[i])
 			if temp != nil {
-				temp.SendText("新闻早操")
+				temp.SendText(newsStr)
 				time.Sleep(5 * time.Second)
 			}
 		}
@@ -110,9 +122,10 @@ func sendNewsTask() {
 
 		temps := strings.Split(sendName, ";")
 		for i := 0; i < len(temps); i++ {
-			temp := groups.GetByRemarkName(temps[i])
+			log.Printf("----------->send group news, %s", temps[i])
+			temp := groups.GetByNickName(temps[i])
 			if temp != nil {
-				temp.SendText("新闻早操")
+				temp.SendText(newsStr)
 				time.Sleep(5 * time.Second)
 			}
 		}
@@ -122,4 +135,43 @@ func sendNewsTask() {
 // NewCommandConfigNewsHandler 创建新闻配置处理器
 func NewCommandConfigNewsHandler() CommandHandlerInterface {
 	return &CommandConfigNewsHandler{}
+}
+
+// RemoteConfigResponseBody 远程config.json参数体
+type NewsResp struct {
+	Code      int `json:"code"`
+	Msg    string `json:"msg"`
+	Data string `json:"data"`
+}
+
+// 新闻
+func RemoteNews() string {
+
+	resp, err := http.Get(config.Config.NewsApi)
+
+	if err != nil {
+		return ""
+	}
+
+	defer resp.Body.Close()
+	respBody, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return ""
+	}
+
+	if resp.StatusCode != 200 {
+		return ""
+	}
+
+	newResp := &NewsResp{}
+	errParse := json.Unmarshal(respBody, newResp)
+	if errParse != nil {
+		log.Printf("error: " + errParse.Error())
+		return ""
+	}
+
+	log.Printf("msg: %s, data: %s", newResp.Msg, newResp.Data)
+
+	return newResp.Data
 }
